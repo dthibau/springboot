f2a81646444e37a041d12e858547a791794a6ed3
David THIBAU <david.thibau@gmail.com>
2020-07-17 11:07:17 +0200

    Securite Stateful

---
+++ demo-workspace/demo-3/src/main/java/org/formation/SecurityBaseConfig.java
@@ -0,0 +1,30 @@
+package org.formation;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityBaseConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/webjars/**","/swagger-resources/**","/**/favicon.*");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/swagger-ui.html","/v2/api-docs").permitAll()
+			.antMatchers(HttpMethod.GET,"/api/**").authenticated()
+			.antMatchers("/api/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+//			.and()
+//			.httpBasic()
+			.and().formLogin().permitAll()
+			.and()
+			.logout().permitAll().invalidateHttpSession(true);
+	}
+}
---
+++ demo-workspace/demo-3/src/main/java/org/formation/service/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package org.formation.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.formation.model.Member;
+import org.formation.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("domain")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	MemberRepository memberRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		if ( email.equals("admin") ) {
+			Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+			grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+			return new User("admin", "{noop}admin", grantedAuthorities);
+		} else {
+		
+			Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+			Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+			grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+			
+			return new User(member.getEmail(), "{noop}" + member.getPassword(),
+					grantedAuthorities);
+		}
+	}
+
+}
