30de4676ec7fdd8e1d60670c1a6906ba2d80ba21
David THIBAU <david.thibau@gmail.com>
2021-01-17 19:27:04 +0100

    SpringSecurity

--- TPs/4_SpringSecurity/Rest/JWT/java/org/formation/controller/UserJWTController.java
+++ TPs/4_SpringSecurity/Rest/JWT/java/org/formation/controller/UserJWTController.java
@@ -12,9 +12,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.annotation.Timed;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,3 +70,4 @@ public class UserJWTController {
         }
     }
 }
+
